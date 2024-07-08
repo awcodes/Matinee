2fb3ee21a6558b102d14b8ea11e5179ea1c77402
PrashantDhungana <sharma.prashant2000@gmail.com>
2024-07-08 23:06:35 +1000

    Added Support for Youtube Shorts

--- src/Providers/YoutubeProvider.php
+++ src/Providers/YoutubeProvider.php
@@ -42,8 +42,11 @@ class YoutubeProvider implements Contracts\MatineeProvider
 
         if (Str::of($this->url)->contains('youtu.be')) {
             $id = Str::of($this->url)->after('youtu.be/');
-        } else {
+        } elseif (Str::of($this->url)->contains('/v=')){
             preg_match('/v=([-\w]+)/', $this->url, $matches);
+            $id = $matches[1] ?? null;
+        } else {
+            preg_match('/shorts\/([-\w]+)/', $this->url, $matches);
             $id = $matches[1] ?? null;
         }
 
