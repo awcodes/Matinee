02a87abb30764458b672524421a6679f40a9d31e
Eduardo Coello <eduardongua@hotmail.com>
2026-01-14 16:23:40 -0700

    Add Spanish translations

---
+++ resources/lang/es/matinee.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'url' => 'URL',
+    'width' => 'Ancho',
+    'height' => 'Alto',
+    'responsive' => 'Responsivo',
+    'responsive_helper' => 'Si el video no es responsivo, establece el ancho y alto en los píxeles reales que debe tener el video. El valor por defecto es 640x480.',
+    'autoplay' => 'Reproducción automática',
+    'loop' => 'Repetir',
+    'title' => 'Título',
+    'byline' => 'Autor',
+    'portrait' => 'Retrato',
+    'controls' => 'Controles',
+    'nocookie' => 'Sin cookies',
+    'start_at' => 'Iniciar en',
+    'local' => 'Local',
+    'preview' => 'Vista previa',
+    'show_preview' => 'Mostrar vista previa',
+    'hide_preview' => 'Ocultar vista previa',
+    'options' => 'Opciones',
+    'invalid_url' => 'No existe un proveedor compatible para esta URL.',
+];
